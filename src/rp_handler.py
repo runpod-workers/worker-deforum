ffc55a7b31c6a796c57f7f7e858be4c30c388433
Justin Merrell <merrelljustin@gmail.com>
2023-06-23 17:45:20 -0400

    Update rp_handler.py

--- src/rp_handler.py
+++ src/rp_handler.py
@@ -23,45 +23,42 @@ def handler(event):
     if (validated_input := validate(_input, INPUT_SCHEMA)).get("errors") is not None:
         return validated_input['errors']
 
-    if (s3_config := event.get("s3Config")) is None:
-        return {
-            "error": "S3_CONFIG_NOT_PROVIDED",
-        }
-    if (s3_access_key_id := s3_config.get("accessId")) is None:
-        return {"error": "AWS_ACCESS_KEY_ID_NOT_PROVIDED"}
-    if (s3_secret_access_key := s3_config.get("accessSecret")) is None:
-        return {
-            "error": "AWS_SECRET_ACCESS_KEY_NOT_PROVIDED",
-        }
-    if (s3_bucket_name := s3_config.get("bucketName")) is None:
-        return {
-            "error": "BUCKET_NOT_PROVIDED",
-        }
+    # if (s3_config := event.get("s3Config")) is None:
+    #     return {
+    #         "error": "S3_CONFIG_NOT_PROVIDED",
+    #     }
+    # if (s3_access_key_id := s3_config.get("accessId")) is None:
+    #     return {"error": "AWS_ACCESS_KEY_ID_NOT_PROVIDED"}
+    # if (s3_secret_access_key := s3_config.get("accessSecret")) is None:
+    #     return {
+    #         "error": "AWS_SECRET_ACCESS_KEY_NOT_PROVIDED",
+    #     }
+    # if (s3_bucket_name := s3_config.get("bucketName")) is None:
+    #     return {
+    #         "error": "BUCKET_NOT_PROVIDED",
+    #     }
 
-    s3_endpoint_url = s3_config.get("endpointUrl")
-    s3_endpoint_url = (
-        s3_endpoint_url if s3_endpoint_url else "https://s3.amazonaws.com/"
-    )
+    # s3_endpoint_url = s3_config.get("endpointUrl")
+    # s3_endpoint_url = (
+    #     s3_endpoint_url if s3_endpoint_url else "https://s3.amazonaws.com/"
+    # )
 
     generated_video_local_path = generate_video.predict(**validated_input['validated_input'])
 
     file_url = upload_file_to_bucket(
         file_name=f"{uuid.uuid4()}.mp4",
         file_location=generated_video_local_path,
-        bucket_creds={
-            "endpointUrl": s3_endpoint_url,
-            "accessId": s3_access_key_id,
-            "accessSecret": s3_secret_access_key,
-        },
-        bucket_name=s3_bucket_name,
+        # bucket_creds={
+        #     "endpointUrl": s3_endpoint_url,
+        #     "accessId": s3_access_key_id,
+        #     "accessSecret": s3_secret_access_key,
+        # },
+        # bucket_name=s3_bucket_name,
     )
 
     os.remove(generated_video_local_path)
 
-    return {
-        "version": "1.06.2023-08:08",
-        "file_url": file_url,
-    }
+    return {"video": file_url}
 
 
 runpod.serverless.start({"handler": handler})
